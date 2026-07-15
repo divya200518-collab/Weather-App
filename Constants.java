dad38d8f5b9810917b5df8187d45b1069efb2a63
divya200518-collab <divya200518@gmail.com>
2026-07-16 03:26:20 +0530

    Constants class

---
+++ Constants.java
@@ -0,0 +1,12 @@
+public class Constants {
+
+    // Replace with your own API key
+    public static final String API_KEY = "YOUR_API_KEY";
+
+    // OpenWeather API URL
+    public static final String BASE_URL =
+            "https://api.openweathermap.org/data/2.5/weather";
+
+    // Temperature unit
+    public static final String UNIT = "metric";
+}
